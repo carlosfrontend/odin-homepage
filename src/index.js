6bd6211ac65799a06984b70f3c4eead349a7346b
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-08-10 17:12:42 +0200

    Feat: Completed mobile header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './css/style.css';
+
+import womanWithLaptop from './images/woman_with_laptop.jpeg';
+
+
+const headerImg = document.querySelector('.woman_with_laptop');
+
+headerImg.src = womanWithLaptop;
