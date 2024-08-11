b8e3751a913ad1e1dd4ea11a01411011914c020f
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-08-12 00:25:20 +0200

    Feat: Rewrite the desktop header

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import './css/style.css';
 
 import womanWithLaptop from './images/woman_with_laptop.jpeg';
+import openInNewLogo from './images/open-in-new.svg';
+import githubLogo from './images/github.svg';
 
 
 const headerImg = document.querySelector('.woman_with_laptop');
+const externalLinkLogo = document.querySelector('.external-link');
+const githubLinkLogo = document.querySelector('.github');
 
 headerImg.src = womanWithLaptop;
+/* externalLinkLogo.src = openInNewLogo;
+githubLinkLogo.src = githubLogo; */
