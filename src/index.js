60902017508566187b8317b733cb7f26c830b0c7
carlosfrontend <11649745+carlosfrontend@users.noreply.github.com>
2024-08-12 11:51:44 +0200

    Feat: Rewrite the desktop design

--- src/index.js
+++ src/index.js
@@ -3,12 +3,29 @@ import './css/style.css';
 import womanWithLaptop from './images/woman_with_laptop.jpeg';
 import openInNewLogo from './images/open-in-new.svg';
 import githubLogo from './images/github.svg';
+import phoneLogo from './images/phone.svg';
+import emailLogo from './images/email-edit.svg';
+import womanWithPhone from './images/woman_with_phone.jpeg';
 
 
 const headerImg = document.querySelector('.woman_with_laptop');
-const externalLinkLogo = document.querySelector('.external-link');
-const githubLinkLogo = document.querySelector('.github');
+const githubLinkLogo = [...document.querySelectorAll('.github')];
+const externalLinkLogo = [...document.querySelectorAll('.external-link')];
+const phoneLinkLogo = document.querySelector('.phone > img');
+const emailLinkLogo = document.querySelector('.email > img');
+const candidatePicture = document.querySelector('.candidate-picture img');
 
 headerImg.src = womanWithLaptop;
-/* externalLinkLogo.src = openInNewLogo;
-githubLinkLogo.src = githubLogo; */
+
+githubLinkLogo.forEach(el => {
+  const element = el;
+  element.src = githubLogo;
+});
+externalLinkLogo.forEach(el => {
+  const element = el;
+  element.src = openInNewLogo;
+});
+
+phoneLinkLogo.src = phoneLogo;
+emailLinkLogo.src = emailLogo;
+candidatePicture.src = womanWithPhone;
